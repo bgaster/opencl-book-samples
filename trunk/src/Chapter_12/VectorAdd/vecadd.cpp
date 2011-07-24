febc9a24eb29e38a4f6bae431da2ac41cb160385
dginsburg@upsamplesoftware.com <dginsburg@upsamplesoftware.com@2820ea6e-710e-dc75-4c53-b434b6cc206b>
2011-07-24 20:44:02 +0000

    Fix more Windows build issues.  Include D3DInterop, add cl.hpp and cl_d3d10.h khronos headers.

--- trunk/src/Chapter_12/VectorAdd/vecadd.cpp
+++ trunk/src/Chapter_12/VectorAdd/vecadd.cpp
@@ -17,11 +17,7 @@
 #define __CL_ENABLE_EXCEPTIONS
 
 
-#if defined(__APPLE__) || defined(__MACOSX)
-#include <OpenCL/cl.hpp>
-#else
 #include <CL/cl.hpp>
-#endif
 
 #include <cstdio>
 #include <cstdlib>
