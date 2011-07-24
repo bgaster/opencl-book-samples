783bb663547c8e9d1b0c61f8098904164fc17dab
dginsburg@upsamplesoftware.com <dginsburg@upsamplesoftware.com@2820ea6e-710e-dc75-4c53-b434b6cc206b>
2011-07-24 20:44:02 +0000

    Fix more Windows build issues.  Include D3DInterop, add cl.hpp and cl_d3d10.h khronos headers.

--- src/Chapter_12/Sinewave/sinewave.cpp
+++ src/Chapter_12/Sinewave/sinewave.cpp
@@ -25,11 +25,11 @@
 
 #define __CL_ENABLE_EXCEPTIONS
 
+#include <CL/cl.hpp>
+
 #if defined(__APPLE__) || defined(__MACOSX)
-#include <OpenCL/cl.hpp>
 #include <OpenGL/opengl.h>
 #else
-#include <CL/cl.hpp>
 #include <GL/gl.h>
 #endif
 
